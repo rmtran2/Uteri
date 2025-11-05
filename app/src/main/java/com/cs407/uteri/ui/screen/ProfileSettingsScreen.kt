b5bf2ac7c7d92279e833c72cf077c95234e5023a
bfleming6 <bfleming6@wisc.edu>
2025-11-05 16:51:18 -0600

    progress commit :)

--- app/src/main/java/com/cs407/uteri/ui/screen/ProfileSettingsScreen.kt
+++ app/src/main/java/com/cs407/uteri/ui/screen/ProfileSettingsScreen.kt
@@ -1,27 +1,39 @@
 package com.cs407.uteri.ui.screen
 
+import android.widget.ToggleButton
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.cs407.uteri.data.ProfileSettingsStorage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun ProfileSettingsScreen() {
     Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text("Profile Settings") } ) },
         bottomBar = { BottomNavBar() }
     ) {innerPadding ->
-        Text(text = "Hi", modifier = Modifier.padding(innerPadding))
+        ProfileSettings(modifier = Modifier.padding(innerPadding))
     }
 }
 
@@ -29,8 +41,29 @@ fun ProfileSettingsScreen() {
 fun ProfileSettings(
     modifier: Modifier = Modifier
 ) {
-    val store = ProfileSettingsStorage(LocalContext.current)
-    val profileSettings = store.profileSettingsFlow.collectAsStateWithLifecycle(ProfileSettings())
-    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+//    val store = ProfileSettingsStorage(LocalContext.current)
+//    val profileSettings = store.profileSettingsFlow.collectAsStateWithLifecycle(ProfileSettings())
+//    val scope = rememberCoroutineScope()
+
+    Column(modifier = modifier) {
+        Spacer(Modifier.padding(50.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+
+            ) {
+                Text(text = "Biometric/Password Enabled", fontWeight = FontWeight.Bold)
+                Text(text = "Require authentication to enter this app")
+            }
+            Switch(
+                checked = false,
+                onCheckedChange = {  },
+            )
+        }
+    }
 
 }
