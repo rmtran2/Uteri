8bcba228be2b70f34fdff0a93f724fba67c099e8
rmtran2 <rmtran2@wisc.edu>
2025-11-05 17:08:51 -0600

    Navigation complete, profile icon added

--- app/src/main/java/com/cs407/uteri/ui/screen/ProfileSettingsScreen.kt
+++ app/src/main/java/com/cs407/uteri/ui/screen/ProfileSettingsScreen.kt
@@ -1,2 +1,14 @@
 package com.cs407.uteri.ui.screen
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileSetingsScreen(
+    onNavigateBack: () -> Unit
+){
+    Text(text = "this is the profile", fontSize = 90.sp)
+}
+
+
