8b99bcbe99e4dfb56e27227b8d6747bd26324f73
Alice Yang <alicyangs@gmail.com>
2025-11-06 18:27:54 -0600

    adding search bar + default location to map

--- app/src/main/java/com/cs407/uteri/ui/screen/ResourceMapScreen.kt
+++ app/src/main/java/com/cs407/uteri/ui/screen/ResourceMapScreen.kt
@@ -3,18 +3,39 @@ package com.cs407.uteri.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.cs407.uteri.ui.utils.Navbar
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ResourceMapScreen(
     onNavigateBack: () -> Unit,
     navController: NavController
 ){
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(LatLng(43.0731, -89.4012), 15f)
+    }
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    var query by remember { mutableStateOf("") }
+
     Scaffold (
         bottomBar = {
             Navbar(navController)
@@ -23,8 +44,29 @@ fun ResourceMapScreen(
         Column(
             modifier = Modifier.padding(padding)
         ) {
+            SearchBar(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                inputField = {
+                    SearchBarDefaults.InputField(
+                        query = query,
+                        onQueryChange = { query = it },
+                        onSearch = {
+                            expanded = false
+                        },
+                        expanded = expanded,
+                        onExpandedChange = { expanded = it },
+                        placeholder = { Text("Search") }
+                    )
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+            ) {
+                Text("TODO.")
+            }
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState
             )
         }
     }
